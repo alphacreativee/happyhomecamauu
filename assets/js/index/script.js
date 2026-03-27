c694ff269fcfe0a48b62a7d75b22efa97ce2cbeb
TAOX3 <tngochai04@gmail.com>
2026-03-27 10:56:53 +0700

    add new src

---
+++ assets/js/index/script.js
@@ -0,0 +1,40 @@
+import { preloadImages } from "../../main/js/utils.min.js";
+import { customDropdown, createFilterTab } from "../../main/js/global.min.js";
+("use strict");
+$ = jQuery;
+
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  customDropdown();
+  createFilterTab();
+};
+preloadImages("img").then(() => {
+  init();
+});
+
+// event click element a
+let isLinkClicked = false;
+
+document.addEventListener("click", (e) => {
+  const link = e.target.closest("a");
+  if (
+    link?.href &&
+    !link.href.startsWith("#") &&
+    !link.href.startsWith("javascript:")
+  ) {
+    isLinkClicked = true;
+  }
+});
+
+window.addEventListener("beforeunload", () => {
+  if (!isLinkClicked) window.scrollTo(0, 0);
+  isLinkClicked = false;
+});
