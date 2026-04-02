a5ef46786f1a322b2a0764b93c006d1fbb37017a
TAOX3 <tngochai04@gmail.com>
2026-04-02 15:59:37 +0700

    add new code

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -479,35 +479,35 @@ function citySection() {
 }
 function mapAnimation() {
   if (!document.querySelector(".map")) return;
-  const runner = document.querySelector(".field-dat-runner");
-  const fill = document.querySelector(".field-dat");
-  const totalLength = runner.getTotalLength();
-  const runnerLength = 60;
-  const DURATION = 2.5;
+  // const runner = document.querySelector(".field-dat-runner");
+  // const fill = document.querySelector(".field-dat");
+  // const totalLength = runner.getTotalLength();
+  // const runnerLength = 60;
+  // const DURATION = 2.5;
 
-  gsap.set(runner, {
-    strokeDasharray: `${runnerLength} ${totalLength - runnerLength}`,
-    strokeDashoffset: 0,
-  });
+  // gsap.set(runner, {
+  //   strokeDasharray: `${runnerLength} ${totalLength - runnerLength}`,
+  //   strokeDashoffset: 0,
+  // });
 
-  gsap.to(runner, {
-    strokeDashoffset: -totalLength,
-    duration: DURATION,
-    ease: "none",
-    repeat: -1,
-  });
+  // gsap.to(runner, {
+  //   strokeDashoffset: -totalLength,
+  //   duration: 4,
+  //   ease: "none",
+  //   repeat: -1,
+  // });
 
-  gsap.fromTo(
-    fill,
-    { fillOpacity: 0.45 },
-    {
-      fillOpacity: 1,
-      duration: DURATION / 2,
-      ease: "sine.inOut",
-      repeat: -1,
-      yoyo: true,
-    },
-  );
+  // gsap.fromTo(
+  //   fill,
+  //   { fillOpacity: 0.45 },
+  //   {
+  //     fillOpacity: 1,
+  //     duration: DURATION / 2,
+  //     ease: "sine.inOut",
+  //     repeat: -1,
+  //     yoyo: true,
+  //   },
+  // );
   const tlMap = gsap.timeline({
     scrollTrigger: {
       trigger: ".map",
